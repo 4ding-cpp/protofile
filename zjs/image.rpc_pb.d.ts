225f2b113e544eda62b3d87a591d3865ce674c7e
4ding-cp <4dingsite@gmail.com>
2020-04-07 11:18:25 +0800

    zjs

--- zjs/image.rpc_pb.d.ts
+++
@@ -1,5 +0,0 @@
-import * as jspb from "google-protobuf"
-
-import * as sql_pb from './sql_pb';
-import * as image_pb from './image_pb';
-
--- zjs/message.rpc_pb.d.ts
+++
@@ -1,6 +0,0 @@
-import * as jspb from "google-protobuf"
-
-import * as message$letter_pb from './message-letter_pb';
-import * as message$edm_pb from './message-edm_pb';
-import * as google_protobuf_wrappers_pb from 'google-protobuf/google/protobuf/wrappers_pb';
-
