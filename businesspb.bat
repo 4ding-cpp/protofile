a6b16a9f1304206f924713f0a2c293d73bcb3c7d
4ding-cp <4dingsite@gmail.com>
2020-04-30 10:04:53 +0800

    c

--- businesspb.bat
+++ businesspb.bat
@@ -4,4 +4,4 @@ protoc -I=a-b -I=a-b-c -I=b -I=b-c -I=rpc-service ^
  --grpc-web_out=import_style=commonjs,mode=grpcweb:./jspb/businesspb ^
  a-b/*.proto a-b-c/*.proto b/*.proto b-c/*.proto ^
  rpc-service/business.rpc.proto ^
- rpc-service/store.rpc.proto
+ rpc-service/store1.rpc.proto
--- customerpb.bat
+++
@@ -1,6 +0,0 @@
-::customerpb
-protoc -I=a-b-c -I=b-c -I=c -I=rpc-service ^
- --js_out=import_style=commonjs,binary:./jspb/customerpb ^
- --grpc-web_out=import_style=commonjs,mode=grpcweb:./jspb/customerpb ^
- a-b-c/*.proto b-c/*.proto c/*.proto ^
- rpc-service/shop.rpc.proto
