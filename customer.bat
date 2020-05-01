8e5e93b54621333ceb69a9e3a90950593341e66b
4ding-cp <4dingsite@gmail.com>
2020-05-01 12:26:20 +0800

    commomjs

--- customer.bat
+++ customer.bat
@@ -1,6 +1,6 @@
 ::customerpb
 protoc -I=a-b-c -I=b-c -I=c -I=rpc-service ^
  --js_out=import_style=commonjs,binary:./jspb/shoppb ^
- --grpc-web_out=import_style=typescript,mode=grpcweb:./jspb/shoppb ^
+ --grpc-web_out=import_style=commonjs,mode=grpcweb:./jspb/shoppb ^
  a-b-c/*.proto b-c/*.proto c/*.proto ^
  rpc-service/shop.rpc.proto
