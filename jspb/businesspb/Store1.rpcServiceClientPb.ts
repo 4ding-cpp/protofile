a3f0fd2ee99463620ba7ca9126219ab51fc82094
4ding-cp <4dingsite@gmail.com>
2021-02-09 17:14:25 +0800

    popl

--- jspb/businesspb/Store1.rpcServiceClientPb.ts
+++ jspb/businesspb/Store1.rpcServiceClientPb.ts
@@ -14,28 +14,28 @@
 import * as grpcWeb from 'grpc-web';
 
 import * as sql_pb from './sql_pb';
-import * as message_pb from './message_pb';
-import * as label_pb from './label_pb';
 import * as pingpong_pb from './pingpong_pb';
-import * as domain_pb from './domain_pb';
 import * as template_pb from './template_pb';
+import * as message_pb from './message_pb';
+import * as domain_pb from './domain_pb';
+import * as label_pb from './label_pb';
 import * as setting_pb from './setting_pb';
 import * as browse_pb from './browse_pb';
 import * as password_pb from './password_pb';
-import * as order_pb from './order_pb';
+import * as manager_pb from './manager_pb';
 import * as activity$coupon_pb from './activity-coupon_pb';
 import * as customer_pb from './customer_pb';
-import * as manager_pb from './manager_pb';
+import * as car_pb from './car_pb';
 import * as adapter_pb from './adapter_pb';
 import * as freeback_pb from './freeback_pb';
-import * as car_pb from './car_pb';
-import * as customer$level_pb from './customer-level_pb';
-import * as sales_pb from './sales_pb';
 import * as product$shell_pb from './product-shell_pb';
-import * as webpage_pb from './webpage_pb';
+import * as customer$level_pb from './customer-level_pb';
+import * as product$class_pb from './product-class_pb';
+import * as sales_pb from './sales_pb';
 import * as website_pb from './website_pb';
 import * as remind_pb from './remind_pb';
-import * as product$class_pb from './product-class_pb';
+import * as webpage_pb from './webpage_pb';
+import * as order_pb from './order_pb';
 
 
 export class Store1RPCClient {
