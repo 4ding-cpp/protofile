2fd7b57dc72cff276bd25ab530daaab5aae31ae1
4ding-cp <4dingsite@gmail.com>
2021-02-09 16:16:55 +0800

    pcachu

--- jspb/businesspb/Business.rpcServiceClientPb.ts
+++ jspb/businesspb/Business.rpcServiceClientPb.ts
@@ -14,12 +14,12 @@
 import * as grpcWeb from 'grpc-web';
 
 import * as sql_pb from './sql_pb';
-import * as domain_pb from './domain_pb';
 import * as business_pb from './business_pb';
 import * as business$sub_pb from './business-sub_pb';
 import * as message_pb from './message_pb';
 import * as label_pb from './label_pb';
 import * as pingpong_pb from './pingpong_pb';
+import * as domain_pb from './domain_pb';
 import * as template_pb from './template_pb';
 import * as point_pb from './point_pb';
 import * as report_pb from './report_pb';
@@ -27,16 +27,16 @@ import * as setting_pb from './setting_pb';
 import * as browse_pb from './browse_pb';
 import * as product_pb from './product_pb';
 import * as password_pb from './password_pb';
+import * as order_pb from './order_pb';
 import * as b2b_pb from './b2b_pb';
 import * as customer_pb from './customer_pb';
-import * as freeback_pb from './freeback_pb';
-import * as purchase_pb from './purchase_pb';
 import * as manager_pb from './manager_pb';
-import * as remind_pb from './remind_pb';
+import * as purchase_pb from './purchase_pb';
+import * as freeback_pb from './freeback_pb';
 import * as customer$level_pb from './customer-level_pb';
-import * as store_pb from './store_pb';
 import * as sales_pb from './sales_pb';
-import * as order_pb from './order_pb';
+import * as store_pb from './store_pb';
+import * as remind_pb from './remind_pb';
 
 
 export class BusinessRPCClient {
--- jspb/shoppb/Shop.rpcServiceClientPb.ts
+++ jspb/shoppb/Shop.rpcServiceClientPb.ts
@@ -17,12 +17,12 @@ import * as sql_pb from './sql_pb';
 import * as order_pb from './order_pb';
 import * as log_pb from './log_pb';
 import * as password_pb from './password_pb';
-import * as activity$coupon_pb from './activity-coupon_pb';
-import * as car_pb from './car_pb';
-import * as freeback_pb from './freeback_pb';
-import * as customer_pb from './customer_pb';
 import * as browse_pb from './browse_pb';
 import * as product$goods_pb from './product-goods_pb';
+import * as activity$coupon_pb from './activity-coupon_pb';
+import * as customer_pb from './customer_pb';
+import * as freeback_pb from './freeback_pb';
+import * as car_pb from './car_pb';
 
 
 export class ShopRPCClient {
