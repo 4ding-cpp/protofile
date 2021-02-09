0446a209b2c015402bdd340feedd65926597c7ee
4ding-cp <4dingsite@gmail.com>
2021-02-09 15:58:49 +0800

    kji

--- jspb/businesspb/Business.rpcServiceClientPb.ts
+++ jspb/businesspb/Business.rpcServiceClientPb.ts
@@ -14,28 +14,28 @@
 import * as grpcWeb from 'grpc-web';
 
 import * as sql_pb from './sql_pb';
-import * as business$sub_pb from './business-sub_pb';
-import * as label_pb from './label_pb';
-import * as message_pb from './message_pb';
-import * as template_pb from './template_pb';
-import * as pingpong_pb from './pingpong_pb';
-import * as business_pb from './business_pb';
 import * as domain_pb from './domain_pb';
+import * as business_pb from './business_pb';
+import * as business$sub_pb from './business-sub_pb';
+import * as message_pb from './message_pb';
+import * as label_pb from './label_pb';
+import * as pingpong_pb from './pingpong_pb';
+import * as template_pb from './template_pb';
 import * as point_pb from './point_pb';
 import * as report_pb from './report_pb';
-import * as password_pb from './password_pb';
 import * as setting_pb from './setting_pb';
 import * as browse_pb from './browse_pb';
 import * as product_pb from './product_pb';
+import * as password_pb from './password_pb';
 import * as b2b_pb from './b2b_pb';
-import * as manager_pb from './manager_pb';
 import * as customer_pb from './customer_pb';
-import * as purchase_pb from './purchase_pb';
 import * as freeback_pb from './freeback_pb';
-import * as store_pb from './store_pb';
-import * as customer$level_pb from './customer-level_pb';
-import * as sales_pb from './sales_pb';
+import * as purchase_pb from './purchase_pb';
+import * as manager_pb from './manager_pb';
 import * as remind_pb from './remind_pb';
+import * as customer$level_pb from './customer-level_pb';
+import * as store_pb from './store_pb';
+import * as sales_pb from './sales_pb';
 import * as order_pb from './order_pb';
 
 
@@ -4176,6 +4176,46 @@ export class BusinessRPCClient {
     request,
     metadata || {},
     this.methodInfoCreateReturn);
+  }
+
+  methodInfoFindOrderFlow = new grpcWeb.AbstractClientBase.MethodInfo(
+    sql_pb.Response,
+    (request: sql_pb.Query) => {
+      return request.serializeBinary();
+    },
+    sql_pb.Response.deserializeBinary
+  );
+
+  findOrderFlow(
+    request: sql_pb.Query,
+    metadata: grpcWeb.Metadata | null): Promise<sql_pb.Response>;
+
+  findOrderFlow(
+    request: sql_pb.Query,
+    metadata: grpcWeb.Metadata | null,
+    callback: (err: grpcWeb.Error,
+               response: sql_pb.Response) => void): grpcWeb.ClientReadableStream<sql_pb.Response>;
+
+  findOrderFlow(
+    request: sql_pb.Query,
+    metadata: grpcWeb.Metadata | null,
+    callback?: (err: grpcWeb.Error,
+               response: sql_pb.Response) => void) {
+    if (callback !== undefined) {
+      return this.client_.rpcCall(
+        this.hostname_ +
+          '/ding4.BusinessRPC/FindOrderFlow',
+        request,
+        metadata || {},
+        this.methodInfoFindOrderFlow,
+        callback);
+    }
+    return this.client_.unaryCall(
+    this.hostname_ +
+      '/ding4.BusinessRPC/FindOrderFlow',
+    request,
+    metadata || {},
+    this.methodInfoFindOrderFlow);
   }
 
   methodInfoUpgradeDomain = new grpcWeb.AbstractClientBase.MethodInfo(
