ae02553d639c062b9f04168c14de5debdc75c346
4ding-cp <4dingsite@gmail.com>
2021-02-24 22:11:22 +0800

    events

---
+++ jspb/adminpb/store_pb.d.ts
@@ -0,0 +1,93 @@
+import * as jspb from "google-protobuf"
+
+import * as sql_pb from './sql_pb';
+import * as manager_pb from './manager_pb';
+import * as google_protobuf_struct_pb from 'google-protobuf/google/protobuf/struct_pb';
+import * as google_protobuf_timestamp_pb from 'google-protobuf/google/protobuf/timestamp_pb';
+
+export class Store extends jspb.Message {
+  getStoreId(): string;
+  setStoreId(value: string): void;
+
+  getBusinessId(): string;
+  setBusinessId(value: string): void;
+
+  getModel(): number;
+  setModel(value: number): void;
+
+  getType(): number;
+  setType(value: number): void;
+
+  getName(): string;
+  setName(value: string): void;
+
+  getUrn(): string;
+  setUrn(value: string): void;
+
+  getIsOpen(): boolean;
+  setIsOpen(value: boolean): void;
+
+  getManager(): manager_pb.Manager | undefined;
+  setManager(value?: manager_pb.Manager): void;
+  hasManager(): boolean;
+  clearManager(): void;
+
+  getLabelxMap(): jspb.Map<string, number>;
+  clearLabelxMap(): void;
+
+  getOperator(): string;
+  setOperator(value: string): void;
+
+  getCreateAt(): google_protobuf_timestamp_pb.Timestamp | undefined;
+  setCreateAt(value?: google_protobuf_timestamp_pb.Timestamp): void;
+  hasCreateAt(): boolean;
+  clearCreateAt(): void;
+
+  getUpdateAt(): google_protobuf_timestamp_pb.Timestamp | undefined;
+  setUpdateAt(value?: google_protobuf_timestamp_pb.Timestamp): void;
+  hasUpdateAt(): boolean;
+  clearUpdateAt(): void;
+
+  getColsList(): Array<string>;
+  setColsList(value: Array<string>): void;
+  clearColsList(): void;
+  addCols(value: string, index?: number): void;
+
+  getConditionList(): Array<sql_pb.Condition>;
+  setConditionList(value: Array<sql_pb.Condition>): void;
+  clearConditionList(): void;
+  addCondition(value?: sql_pb.Condition, index?: number): sql_pb.Condition;
+
+  getSelf(): google_protobuf_struct_pb.Struct | undefined;
+  setSelf(value?: google_protobuf_struct_pb.Struct): void;
+  hasSelf(): boolean;
+  clearSelf(): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): Store.AsObject;
+  static toObject(includeInstance: boolean, msg: Store): Store.AsObject;
+  static serializeBinaryToWriter(message: Store, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): Store;
+  static deserializeBinaryFromReader(message: Store, reader: jspb.BinaryReader): Store;
+}
+
+export namespace Store {
+  export type AsObject = {
+    storeId: string,
+    businessId: string,
+    model: number,
+    type: number,
+    name: string,
+    urn: string,
+    isOpen: boolean,
+    manager?: manager_pb.Manager.AsObject,
+    labelxMap: Array<[string, number]>,
+    operator: string,
+    createAt?: google_protobuf_timestamp_pb.Timestamp.AsObject,
+    updateAt?: google_protobuf_timestamp_pb.Timestamp.AsObject,
+    colsList: Array<string>,
+    conditionList: Array<sql_pb.Condition.AsObject>,
+    self?: google_protobuf_struct_pb.Struct.AsObject,
+  }
+}
+
