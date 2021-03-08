25776ef6ef213b6a7e4ac0137c239d985e6bd108
4ding-cp <4dingsite@gmail.com>
2021-03-08 15:31:41 +0800

    ug

---
+++ jspb/businesspb/user-group_pb.d.ts
@@ -0,0 +1,62 @@
+import * as jspb from "google-protobuf"
+
+import * as sql_pb from './sql_pb';
+import * as google_protobuf_struct_pb from 'google-protobuf/google/protobuf/struct_pb';
+import * as google_protobuf_timestamp_pb from 'google-protobuf/google/protobuf/timestamp_pb';
+
+export class UserGroup extends jspb.Message {
+  getGroupid(): string;
+  setGroupid(value: string): void;
+
+  getName(): string;
+  setName(value: string): void;
+
+  getOperator(): string;
+  setOperator(value: string): void;
+
+  getCreateAt(): google_protobuf_timestamp_pb.Timestamp | undefined;
+  setCreateAt(value?: google_protobuf_timestamp_pb.Timestamp): void;
+  hasCreateAt(): boolean;
+  clearCreateAt(): void;
+
+  getUpdateAt(): google_protobuf_timestamp_pb.Timestamp | undefined;
+  setUpdateAt(value?: google_protobuf_timestamp_pb.Timestamp): void;
+  hasUpdateAt(): boolean;
+  clearUpdateAt(): void;
+
+  getColsList(): Array<string>;
+  setColsList(value: Array<string>): void;
+  clearColsList(): void;
+  addCols(value: string, index?: number): void;
+
+  getConditionList(): Array<sql_pb.Condition>;
+  setConditionList(value: Array<sql_pb.Condition>): void;
+  clearConditionList(): void;
+  addCondition(value?: sql_pb.Condition, index?: number): sql_pb.Condition;
+
+  getSelf(): google_protobuf_struct_pb.Struct | undefined;
+  setSelf(value?: google_protobuf_struct_pb.Struct): void;
+  hasSelf(): boolean;
+  clearSelf(): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): UserGroup.AsObject;
+  static toObject(includeInstance: boolean, msg: UserGroup): UserGroup.AsObject;
+  static serializeBinaryToWriter(message: UserGroup, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): UserGroup;
+  static deserializeBinaryFromReader(message: UserGroup, reader: jspb.BinaryReader): UserGroup;
+}
+
+export namespace UserGroup {
+  export type AsObject = {
+    groupid: string,
+    name: string,
+    operator: string,
+    createAt?: google_protobuf_timestamp_pb.Timestamp.AsObject,
+    updateAt?: google_protobuf_timestamp_pb.Timestamp.AsObject,
+    colsList: Array<string>,
+    conditionList: Array<sql_pb.Condition.AsObject>,
+    self?: google_protobuf_struct_pb.Struct.AsObject,
+  }
+}
+
