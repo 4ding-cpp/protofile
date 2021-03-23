3e718bc0aa3316d3ba5b754b9cc023c319d5bbe1
4ding-cp <4dingsite@gmail.com>
2021-03-23 16:04:42 +0800

    ppk

--- jspb/adminpb/Admin.rpcServiceClientPb.ts
+++ jspb/adminpb/Admin.rpcServiceClientPb.ts
@@ -259,6 +259,28 @@ export class AdminRPCClient {
       callback);
   }
 
+  methodInfoDuplicate = new grpcWeb.AbstractClientBase.MethodInfo(
+    sql_pb.Response,
+    (request: sql_pb.Query) => {
+      return request.serializeBinary();
+    },
+    sql_pb.Response.deserializeBinary
+  );
+
+  duplicate(
+    request: sql_pb.Query,
+    metadata: grpcWeb.Metadata | null,
+    callback: (err: grpcWeb.Error,
+               response: sql_pb.Response) => void) {
+    return this.client_.rpcCall(
+      this.hostname_ +
+        '/ding4.AdminRPC/Duplicate',
+      request,
+      metadata || {},
+      this.methodInfoDuplicate,
+      callback);
+  }
+
   methodInfoCreateAdmin = new grpcWeb.AbstractClientBase.MethodInfo(
     sql_pb.Response,
     (request: admin_pb.Admin) => {
