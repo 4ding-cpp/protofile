750dc09a76da250fccb7e7e88a517b8d3ff190a9
4ding-cp <4dingsite@gmail.com>
2021-03-23 17:06:26 +0800

    qqfgf

--- jspb/businesspb/Business.rpcServiceClientPb.ts
+++ jspb/businesspb/Business.rpcServiceClientPb.ts
@@ -251,6 +251,28 @@ export class BusinessRPCClient {
       callback);
   }
 
+  methodInfoFindInfo = new grpcWeb.AbstractClientBase.MethodInfo(
+    sql_pb.Response,
+    (request: sql_pb.Query) => {
+      return request.serializeBinary();
+    },
+    sql_pb.Response.deserializeBinary
+  );
+
+  findInfo(
+    request: sql_pb.Query,
+    metadata: grpcWeb.Metadata | null,
+    callback: (err: grpcWeb.Error,
+               response: sql_pb.Response) => void) {
+    return this.client_.rpcCall(
+      this.hostname_ +
+        '/ding4.BusinessRPC/FindInfo',
+      request,
+      metadata || {},
+      this.methodInfoFindInfo,
+      callback);
+  }
+
   methodInfoDuplicate = new grpcWeb.AbstractClientBase.MethodInfo(
     sql_pb.Response,
     (request: sql_pb.Query) => {
