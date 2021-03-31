e5b424e7d73720a7fa6c15ab390b91a5deed9d9d
4ding-cp <4dingsite@gmail.com>
2021-03-31 17:29:37 +0800

    otp

--- jspb/businesspb/Store1.rpcServiceClientPb.ts
+++ jspb/businesspb/Store1.rpcServiceClientPb.ts
@@ -184,6 +184,28 @@ export class Store1RPCClient {
       callback);
   }
 
+  methodInfoComfirmOTPToken = new grpcWeb.AbstractClientBase.MethodInfo(
+    sql_pb.Response,
+    (request: manager_pb.Manager) => {
+      return request.serializeBinary();
+    },
+    sql_pb.Response.deserializeBinary
+  );
+
+  comfirmOTPToken(
+    request: manager_pb.Manager,
+    metadata: grpcWeb.Metadata | null,
+    callback: (err: grpcWeb.Error,
+               response: sql_pb.Response) => void) {
+    return this.client_.rpcCall(
+      this.hostname_ +
+        '/ding4.Store1RPC/ComfirmOTPToken',
+      request,
+      metadata || {},
+      this.methodInfoComfirmOTPToken,
+      callback);
+  }
+
   methodInfoFindPermission = new grpcWeb.AbstractClientBase.MethodInfo(
     sql_pb.Response,
     (request: sql_pb.Query) => {
