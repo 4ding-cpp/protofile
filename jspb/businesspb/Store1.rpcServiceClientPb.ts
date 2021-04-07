db28515622a1f0a30b57cb2fe0f6214d8655af51
4ding-cp <4dingsite@gmail.com>
2021-04-07 17:29:59 +0800

    qqghgh

--- jspb/businesspb/Store1.rpcServiceClientPb.ts
+++ jspb/businesspb/Store1.rpcServiceClientPb.ts
@@ -360,6 +360,28 @@ export class Store1RPCClient {
       callback);
   }
 
+  methodInfoFindRecord = new grpcWeb.AbstractClientBase.MethodInfo(
+    sql_pb.Response,
+    (request: sql_pb.Query) => {
+      return request.serializeBinary();
+    },
+    sql_pb.Response.deserializeBinary
+  );
+
+  findRecord(
+    request: sql_pb.Query,
+    metadata: grpcWeb.Metadata | null,
+    callback: (err: grpcWeb.Error,
+               response: sql_pb.Response) => void) {
+    return this.client_.rpcCall(
+      this.hostname_ +
+        '/ding4.Store1RPC/FindRecord',
+      request,
+      metadata || {},
+      this.methodInfoFindRecord,
+      callback);
+  }
+
   methodInfoDashboard = new grpcWeb.AbstractClientBase.MethodInfo(
     sql_pb.Response,
     (request: sql_pb.Query) => {
