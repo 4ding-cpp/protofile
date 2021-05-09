139fd1c63be1610f4afde45ecec90688341c39bb
4ding-cp <4dingsite@gmail.com>
2021-05-09 14:16:06 +0800

    ss

--- businesspb.bat
+++ businesspb.bat
@@ -7,4 +7,5 @@ protoc -I=a-b -I=a-b-c -I=b -I=b-c -I=rpc-service ^
  --grpc-web_out=import_style=typescript,mode=grpcweb:./jspb/businesspb ^
  a-b/*.proto a-b-c/*.proto b/*.proto b-c/*.proto ^
  rpc-service/business.rpc.proto ^
- rpc-service/store1.rpc.proto
+ rpc-service/store1.rpc.proto ^
+ rpc-service/sales.rpc.proto ^
